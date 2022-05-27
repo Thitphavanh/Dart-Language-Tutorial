00204c4e4b0b2baefba6d1eb0464c04dc64c2b2d
Hery <thitphavanh.psv@gmail.com>
2022-05-27 12:09:34 +0700

    update

---
+++ tutorial1/ep6_for_loop.dart
@@ -0,0 +1,7 @@
+// For loop
+
+void main() {
+  for (var i = 0; i < 5; i++) {
+    print('Hello World');
+  }
+}
---
+++ tutorial1/ep7_while_loop.dart
@@ -0,0 +1,14 @@
+// While loop
+
+void main() {
+  //While loop
+  var i = 0;
+  int number = 5;
+  int result;
+
+  while (i < 5) {
+    result = number * i;
+    print(result);
+    i++;
+  }
+}
