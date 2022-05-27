85519d33939e721e574547ab48915305544bf09e
Hery <thitphavanh.psv@gmail.com>
2022-05-27 12:16:46 +0700

    update

---
+++ tutorial1/ep8_do_while_loop.dart
@@ -0,0 +1,11 @@
+// Do while loop
+
+void main() {
+  // Do while loop
+  var i = 0;
+
+  do {
+    print('Hello World');
+    i++;
+  } while (i < 5);
+}
---
+++ tutorial1/ep9_for_in_loop.dart
@@ -0,0 +1,10 @@
+// For in loop
+
+void main() {
+  // For in loop
+  List<String> color = ['Red', 'Blue', 'Black'];
+
+  for (var colors in color) {
+    print(colors);
+  }
+}
